e85101b9875dbfc2adba6f30046b6bd2a6b0fb7c
Ravi Kant <113057392+Ravikantmahi@users.noreply.github.com>
2024-10-09 17:56:46 +0530

    2nd commit

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./components/Footer/Footer";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Projects />
       <Certificate/>
       <Footer/>
+      <Analytics />
     </div>
   );
 }
