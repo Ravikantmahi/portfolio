b660398fb743e27f55131436758309a86efe750b
Ravi Kant <113057392+Ravikantmahi@users.noreply.github.com>
2024-10-10 20:17:48 +0530

    2nd commit

--- myportfolio/src/components/Footer/Footer.jsx
+++ myportfolio/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer" id='contact'>
       <div className="footer-container">
-        <p>&copy; 2024 Your Ravikant. All rights reserved.</p>
+        <p>&copy; 2024 Ravikant. All rights reserved.</p>
         <ul className="social-links">
           <li><a href="https://www.instagram.com/ravikantmahi011/profilecard/?igsh=ajQ1Zjg3ZmpsYnQ3" target="_blank">Instagram</a></li>
           <li><a href="https://www.linkedin.com/in/ravi-k-049bba285?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">LinkedIn</a></li>
--- myportfolio/src/components/Hero/Hero.jsx
+++ myportfolio/src/components/Hero/Hero.jsx
@@ -9,8 +9,9 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Ravi Kant</h1>
         <p className={styles.description}>
-          I'm a Frontend Developer with experience using React and
-          NodeJS. Apart from this, I also know Web Designing and Graphics Designing. Reach out if you'd like to know more!
+          I'm a Frontend Developer with experience using React and NodeJS. Apart
+          from this, I also know Web Designing and Graphics Designing. Reach out
+          if you'd like to know more!
         </p>
         <a href="#contact" className={styles.contactBtn}>
           Contact Me
